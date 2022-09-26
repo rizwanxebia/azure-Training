68a028afab91fd75ebd50a66bcd6df7dcd9d1702
Rizwan <mohammad.rizwan@nokia.com>
2022-09-26 12:17:59 +0530

    azure traning new project

---
+++ src/main/java/com/azure/training/AzureTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.azure.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azure/training/AzureTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azure.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
